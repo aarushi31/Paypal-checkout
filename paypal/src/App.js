be28ce31697df3b2334e051368d3298051d0c358
aarushi31 <aarushishanker3108@gmail.com>
2021-03-04 11:33:28 +0530

    uploading files

---
+++ paypal/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import React from "react";
+import ReactDOM from "react-dom"
+
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+function App() {
+  const createOrder=(data, actions)=> {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "0.01",
+          },
+        },
+      ],
+    });
+  }
+  const onApprove=(data, actions)=>{
+    console.log(data);
+    return actions.order.capture();
+  }
+  return (
+    <div className="App">
+      <div className="wrapper">
+      <PayPalButton
+        createOrder={(data, actions) => createOrder(data, actions)}
+        onApprove={(data, actions) => onApprove(data, actions)}
+      />
+      </div>
+    </div>
+  );
+}
+
+export default App;
